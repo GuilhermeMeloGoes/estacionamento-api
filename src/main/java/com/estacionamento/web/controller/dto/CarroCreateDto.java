190fc47d050942508ea6ae67c28fee7a432b1ea5
joaovirone <jovmamikl@gmail.com>
2026-01-18 15:32:23 -0300

    feat: implementado DTO da criação do carro utiliza ndo a placa como formato principal MERCOSUL

---
+++ src/main/java/com/estacionamento/web/controller/dto/CarroCreateDto.java
@@ -0,0 +1,22 @@
+package com.estacionamento.web.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CarroCreateDto {
+
+    @NotBlank
+    @Pattern(message = "Formato da placa inválido.", regexp = "^[A-Z]{3}[0-9][A-Z][0-9]{2}$")
+    @Size(min = 7, max = 7)
+    private String placaCArro;    
+
+
+    
+}
