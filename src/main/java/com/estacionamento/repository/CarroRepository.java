f61dc09903bfea372181fad979de30574a6c5f66
joaovirone <jovmamikl@gmail.com>
2026-01-18 16:14:54 -0300

    fix: ajustado método de buscar carro por id para buscar pela placa

--- src/main/java/com/estacionamento/repository/CarroRepository.java
+++ src/main/java/com/estacionamento/repository/CarroRepository.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.estacionamento.entity.Carro;
+import java.util.List;
+
 
 public interface CarroRepository extends JpaRepository<Carro,Long>{
 
-    Optional<Carro> findById(String placaCarro);
+    Optional<Carro> findByPlacaCarro(String placaCarro);
+
     
 }
