3663981d48faa20503b8ee06a2d507d5c1f6a501
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-20 23:16:08 -0300

    feat: criação do cliente repository

---
+++ src/main/java/com/estacionamento/exception/CPFUniqueViolationException.java
@@ -0,0 +1,4 @@
+package com.estacionamento.exception;
+
+public class CPFUniqueViolationException {
+}
---
+++ src/main/java/com/estacionamento/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.estacionamento.repository;
+
+import com.estacionamento.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
