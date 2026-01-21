0e26de5c57b9379446ea2f1f9f0fb67f1a159557
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-20 23:33:53 -0300

    fix: adicionando a anotação noArgsConstructor no Mapper de cliente

--- src/main/java/com/estacionamento/web/controller/dto/mapper/ClienteMapper.java
+++ src/main/java/com/estacionamento/web/controller/dto/mapper/ClienteMapper.java
@@ -3,15 +3,18 @@ package com.estacionamento.web.controller.dto.mapper;
 import com.estacionamento.entity.Cliente;
 import com.estacionamento.web.controller.dto.ClienteResponseDto;
 import com.estacionamento.web.controller.dto.ClienteCreateDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClienteMapper {
 
     public static Cliente toCliente(ClienteCreateDto clienteCreateDto) {
         return new ModelMapper().map(clienteCreateDto, Cliente.class);
     }
 
-    public static ClienteResponseDto toUsuarioResponseDto(Cliente cliente) {
+    public static ClienteResponseDto toClienteResponseDto(Cliente cliente) {
         return new ModelMapper().map(cliente, ClienteResponseDto.class);
     }
 }
