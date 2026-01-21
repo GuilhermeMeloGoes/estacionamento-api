360a5e4275ec7167fb6c1a4669bbb94efdbeb1da
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-21 12:02:26 -0300

    fix: corrigindo o tipo do retorno do id, que estava Integer, mas era para ser Long

--- src/main/java/com/estacionamento/repository/ClienteRepository.java
+++ src/main/java/com/estacionamento/repository/ClienteRepository.java
@@ -3,5 +3,5 @@ package com.estacionamento.repository;
 import com.estacionamento.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
