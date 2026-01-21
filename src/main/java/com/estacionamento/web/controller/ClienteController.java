19757b2d5c3e9d81b2b329d2846b6fe4e20ca974
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-20 23:33:10 -0300

    feat: adcionando o controller de cliente

--- src/main/java/com/estacionamento/web/controller/ClienteController.java
+++ src/main/java/com/estacionamento/web/controller/ClienteController.java
@@ -1,4 +1,30 @@
 package com.estacionamento.web.controller;
 
+import com.estacionamento.entity.Cliente;
+import com.estacionamento.service.ClienteService;
+import com.estacionamento.service.UsuarioService;
+import com.estacionamento.web.controller.dto.ClienteCreateDto;
+import com.estacionamento.web.controller.dto.ClienteResponseDto;
+import com.estacionamento.web.controller.dto.mapper.ClienteMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/clientes")
+@RequiredArgsConstructor
 public class ClienteController {
+
+    private final ClienteService clienteService;
+    private final UsuarioService usuarioService;
+
+    @PostMapping("/{idUsuario}")
+    public ResponseEntity<ClienteResponseDto> cadastrarCliente(@RequestBody @Valid ClienteCreateDto clienteDto, @PathVariable Long idUsuario) {
+        Cliente cliente = ClienteMapper.toCliente(clienteDto);
+        cliente.setUsuario(usuarioService.findByIdUser(idUsuario));
+        clienteService.createClient(cliente);
+
+        return ResponseEntity.status(201).body(ClienteMapper.toClienteResponseDto(cliente));
+    }
 }
