b2c138b1f212d74c59432bf8e131290af40bf8d0
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-21 12:03:24 -0300

    feat: adicionando novas funcionalidades ao controller de cliente

--- src/main/java/com/estacionamento/web/controller/ClienteController.java
+++ src/main/java/com/estacionamento/web/controller/ClienteController.java
@@ -8,8 +8,12 @@ import com.estacionamento.web.controller.dto.ClienteResponseDto;
 import com.estacionamento.web.controller.dto.mapper.ClienteMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("api/v1/clientes")
@@ -27,4 +31,17 @@ public class ClienteController {
 
         return ResponseEntity.status(201).body(ClienteMapper.toClienteResponseDto(cliente));
     }
+
+    @GetMapping("/{idCliente}")
+    public ResponseEntity<ClienteResponseDto> buscarClientePorId(@PathVariable Long idCliente) {
+        Cliente cliente = clienteService.buscarClientePorId(idCliente);
+        return ResponseEntity.status(HttpStatus.OK).body(ClienteMapper.toClienteResponseDto(cliente));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Cliente>> buscarTodosOsClientes(Pageable pageable) {
+        Page<Cliente> clientes = clienteService.buscarTodosOsClientes(pageable);
+        return ResponseEntity.ok(clientes);
+    }
+
 }
