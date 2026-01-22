0505af474a63a313720836b18cada9afb5384491
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-21 23:09:47 -0300

    feat: adicionando novos métodos abstratos na classe repository do cliente

--- src/main/java/com/estacionamento/repository/ClienteRepository.java
+++ src/main/java/com/estacionamento/repository/ClienteRepository.java
@@ -1,7 +1,18 @@
 package com.estacionamento.repository;
 
 import com.estacionamento.entity.Cliente;
+import com.estacionamento.repository.projection.ClienteProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Query("select c from Cliente c")
+    Page<ClienteProjection> findAllPageble(Pageable pageable);
+
+    Cliente findByUsuarioId(Long idUsuario);
 }
