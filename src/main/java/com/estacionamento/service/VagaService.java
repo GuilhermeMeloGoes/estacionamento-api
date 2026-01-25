4c66c0f37eb31fcbdc86107e39181c6357cc1aa5
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-24 21:27:40 -0300

    feat: criando o service da vaga

---
+++ src/main/java/com/estacionamento/service/VagaService.java
@@ -0,0 +1,35 @@
+package com.estacionamento.service;
+
+import com.estacionamento.entity.Vagas;
+import com.estacionamento.exception.CodigoUniqueViolationException;
+import com.estacionamento.exception.EntityNotFoundException;
+import com.estacionamento.repository.VagaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class VagaService {
+
+    private final VagaRepository vagaRepository;
+
+    @Transactional
+    public Vagas criarVaga(Vagas vagas) {
+        try {
+            return vagaRepository.save(vagas);
+        } catch (DataIntegrityViolationException e) {
+            throw new CodigoUniqueViolationException(String.format("Vaga com o código %s, já foi cadastrada!", vagas.getCodigo()));
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Vagas buscarVagaPorCodigo(String codigo) {
+        return vagaRepository.findByCodigo(codigo).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Vaga com o código %s, não encontrada.", codigo))
+        );
+    }
+
+
+}
