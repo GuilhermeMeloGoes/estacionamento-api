e1800dc4f9491d288476f86eeea464e86169bdd8
GuilhermeMeloGoes <guimelogoesDev@gmail.com>
2026-01-27 21:20:51 -0300

    feat: adicionando a classe de entidade carro

--- src/main/java/com/estacionamento/entity/Carro.java
+++ src/main/java/com/estacionamento/entity/Carro.java
@@ -1,20 +1,18 @@
 package com.estacionamento.entity;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "carros")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Carro {
 
     @Id
@@ -22,17 +20,63 @@ public class Carro {
     @Column
     private Long id;
 
-    @Column(name = "marca_carro", nullable = true, length = 100)
-    private String marcaCarro;
+    @Column(name = "numero_recibo", nullable = false, unique = true, length = 15)
+    private String recibo;
 
-    @Column(name = "modelo_carro", nullable = true, length = 100)
-    private String modeloCarro;
-
-    @Column(name = "cor_carro", nullable = true, length = 100)
-    private String corCarro;
-
-    @Column(name = "placa_carro", nullable = false, length = 100)
+    @Column(name = "placa_carro", nullable = false, length = 8)
     private String placaCarro;
 
+    @Column(name = "marca_carro", nullable = false, length = 45)
+    private String marcaCarro;
 
+    @Column(name = "modelo_carro", nullable = false, length = 45)
+    private String modeloCarro;
+
+    @Column(name = "cor_carro", nullable = false, length = 45)
+    private String corCarro;
+
+    @Column(name = "data_entrada", nullable = false)
+    private LocalDateTime dataEntrada;
+
+    @Column(name = "data_saida")
+    private LocalDateTime dataSaida;
+
+    @Column(name = "valor_carro", columnDefinition = "decimal(7,2)")
+    private BigDecimal valor;
+
+    @Column(name = "valor_desconto", columnDefinition = "decimal(7,2)")
+    private BigDecimal valorDesconto;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_vaga", nullable = false)
+    private Vagas vaga;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return Objects.equals(id, carro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
